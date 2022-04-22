68721e10fac1c2ff6acd3a7d1ee3e32df15e2f29
Manuel Sousa <mlrcbsousa@gmail.com>
2022-04-22 20:29:25 +0200

    Complete ex02.

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 17:31:46 by msousa            #+#    #+#             */
-/*   Updated: 2022/04/22 17:33:33 by msousa           ###   ########.fr       */
+/*   Updated: 2022/04/22 20:17:34 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,104 @@
 //                         	Array Class Template                          	  //
 // ************************************************************************** //
 
+template< typename T >
 class Array {
 
 public:
 
-	Array( void );
-	Array( Array const & src );
-	~Array( void );
-	Array &	operator = ( Array const & rhs );
+	class OutOfBoundsException : public std::exception {
+	public:
+		virtual const char*	what( void ) const throw();
+	};
+
+	Array<T>( void );
+	Array<T>( unsigned int n );
+	Array<T>( Array<T> const & src );
+	~Array<T>( void );
+	Array<T> &		operator = ( Array<T> const & rhs );
+
+	T &				operator [] ( int index ) const;
+	unsigned int	size( void ) const;
+
+	T *				getPtr( void ) const;
 
 private:
 
-
+	unsigned int 	n;
+	T *				ptr;
 
 };
 
-std::ostream &	operator << ( std::ostream & o, Array const & i );
+template< typename T >
+const char*	Array<T>::OutOfBoundsException::what( void ) const throw()
+{
+	return "index out of bounds of the array";
+}
+
+/* Constructors */
+template< typename T >
+Array<T>::Array( void ) : n(0), ptr(new T[0]) { /* no-op */ }
+
+template< typename T >
+Array<T>::Array( unsigned int n ) : n(n), ptr(new T[n]) { /* no-op */ }
+
+template< typename T >
+Array<T>::Array( Array<T> const & src ) { *this = src; }
+
+/* Destructor */
+template< typename T >
+Array<T>::~Array( void ) { delete[] ptr; }
+
+/* Assignment operator */
+template< typename T >
+Array<T> &	Array<T>::operator = ( Array<T> const & rhs )
+{
+	if (this != &rhs) {
+		if (ptr)
+			delete[] ptr;
+
+		n = rhs.size();
+		ptr = new T[n];
+
+		T *	copiedPtr = rhs.getPtr();
+		for (uint i = 0; i < n; i++)
+			ptr[i] = copiedPtr[i];
+	}
+	return *this;
+}
+
+/* Subscript operator */
+template< typename T >
+T &	Array<T>::operator [] ( int index ) const
+{
+	if (index + 1 > (int)n || index < 0)
+		throw Array<T>::OutOfBoundsException();
+
+	return ptr[index];
+}
+
+template< typename T >
+unsigned int	Array<T>::size( void ) const { return n; }
+
+template< typename T >
+T *	Array<T>::getPtr( void ) const { return ptr; }
+
+/* ostream override */
+template< typename T >
+std::ostream &	operator << ( std::ostream & o, Array<T> const & array )
+{
+	o << "[ ";
+
+	if (array.size()) {
+		uint i = 0;
+		for (; i < array.size() - 1; i++)
+			o << array[i] << ", ";
+		o << array[i];
+	}
+
+	o << " ]";
+
+	return o;
+}
 
 #endif /* __ARRAY_H__ */
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:20:00 by msousa            #+#    #+#             */
-/*   Updated: 2022/04/22 18:00:50 by msousa           ###   ########.fr       */
+/*   Updated: 2022/04/22 20:28:07 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 int main( void )
 {
 	Array< int > 			numbers(5);
-	Array< std::string > 	strings(1);
+	Array< std::string > 	strings(2);
 	Array< int > 			nothing;
+
+	// default values
+	LOG(numbers);
+	LOG(strings);
+	LOG(nothing);
 
 	// subscript operator []
 	numbers[3] = 42;
 	strings[0] = "Hello world!";
-	LOG(numbers[3]);		// 42
-	LOG(strings[0]);		// Hello world!
+	LOG(numbers);
+	LOG(strings);
 
 	// size
 	LOG(nothing.size());	// 0
 	LOG(numbers.size());	// 5
-	LOG(strings.size());	// 1
+	LOG(strings.size());	// 2
 
 	// assignment operator =
 	nothing = numbers;
